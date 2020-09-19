ee418b4d7f5594423ab1916cfa996a8b32517ef1
Jansmig <jan.smigielski@gmail.com>
2020-09-19 20:48:58 +0200

    initial commit

---
+++ src/main/java/com/manager/front2/Front2Application.java
@@ -0,0 +1,13 @@
+package com.manager.front2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Front2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Front2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/manager/front2/Front2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.manager.front2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Front2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
